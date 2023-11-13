c93760dae0f912fb81c4893d37c61b27ea6e7dd3
j <chrystopherm@icloud.com>
2023-11-12 23:03:55 -0300

    separacao movie

--- src/Requests.js
+++ src/Requests.js
@@ -4,7 +4,7 @@ const requests = {
 
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=pt-BR&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=pt-BR&page=1`,
-    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=pt-BR&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=pt-BR&page=2`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=pt-BR&page=1`,
     requestHorror: `https://api.themoviedb.org/3/movie/horror?api_key=${key}&language=pt-BR&page=1`,
 };
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import { FaHeart, FaRegHeart} from 'react-icons/fa'
+
 import React, { useEffect, useState } from 'react';
+import Movie from './Movie';
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([])
-  const [like, setLike] = useState(false)
+  
 
   useEffect(() => {
     axios.get(fetchURL)
@@ -29,22 +30,9 @@ const Row = ({ title, fetchURL }) => {
     <>
     <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
     <div className='relative flex items-center'>
-        <div id={'slider'}>
+        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
           {movies.map((item, id)=> (
-            <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
-                 alt={item?.title} 
-                />
-              <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                <p className='white-scape-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
-                  {item?.title}
-                </p>
-                <p>
-                  {like ? <FaHeart className='absolute top-4 left-4 text-grey-300'/> : <FaRegHeart className='absolute top-5 left-4 text-grey-300'/>}
-                </p>
-                
-              </div>
-            </div>
+              <Movie key={id} item={item} />
           ))}
 
         </div>
