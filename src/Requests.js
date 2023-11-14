895743f57c3e3a068e32743c9814bdf06683f5fe
j <chrystopherm@icloud.com>
2023-11-14 19:31:54 -0300

    correcao api horror

--- src/Requests.js
+++ src/Requests.js
@@ -6,7 +6,7 @@ const requests = {
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=pt-BR&page=1`,
     requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=pt-BR&page=2`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=pt-BR&page=1`,
-    requestHorror: `https://api.themoviedb.org/3/movie/horror?api_key=${key}&language=pt-BR&page=1`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=pt-BR&query=horror&page=1&include_adult=false`,
 };
 
 export default requests
