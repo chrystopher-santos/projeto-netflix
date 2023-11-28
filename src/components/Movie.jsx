e0c27db476923b5295371354fbba97334a950be5
j <chrystopherm@icloud.com>
2023-11-27 22:48:42 -0300

    lista de favoritos

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,31 @@
 import React, { useState } from 'react';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { UserAuth } from '../context/AuthContext'
+import { db } from '../firebase'
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
 
 const Movie = ({ item }) => {
   const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const { user} = UserAuth();
+
+  const movieID = doc(db, 'users', `${user?.email}`)
+
+  const saveShow = async () => {
+    if(user?.email){
+      setLike(!like)
+      setSaved(true)
+      await updateDoc(movieID, {
+        savedShows: arrayUnion({
+          id: item.id,
+          title: item.title,
+          img: item.backdrop_path
+        })
+      })
+    } else {
+      alert('Por favor, entre para favoritar o filme');
+    }
+  }
 
   return (
     <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
@@ -16,7 +39,7 @@ const Movie = ({ item }) => {
           className='px-2 whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center break-words'>
           {item?.title}
         </p>
-        <p>
+        <p onClick={saveShow}>
           {like ? (
             <FaHeart className='absolute top-4 left-4 text-grey-300' />
           ) : (
